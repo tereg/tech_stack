dc79deaf0cc2b3ec5b2a2cadcfd53263f022c36c
tereg <teregalla@gmail.com>
2017-07-26 13:01:27 -0700

    Create LibraryReducer and create LibraryList json file.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,12 @@
 import { combineReducers } from 'redux';
+import LibraryReducer from './LibraryReducer';
 
 export default combineReducers({
-  libraries: () => []
+  libraries: LibraryReducer
 });
+
+
+// Reducers produce our application state. combineReducers makes multiple reducers work together nicely.
+
+// console.log(store.getState());
+// { libraries: [] }
